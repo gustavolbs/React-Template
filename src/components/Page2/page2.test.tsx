4a3f49fa6734473476759ec173ce0e7b8e4dcda9
Gustavo Bispo <gustavo.santos@ccc.ufcg.edu.br>
2022-03-02 10:43:32 -0300

    fix: test example

--- src/components/Page2/page2.test.tsx
+++ src/components/Page2/page2.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Page2 from "./index";
 
-test("renders learn react link", () => {
+test("renders counter page", () => {
   render(<Page2 />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Counter/i);
   expect(linkElement).toBeInTheDocument();
 });
